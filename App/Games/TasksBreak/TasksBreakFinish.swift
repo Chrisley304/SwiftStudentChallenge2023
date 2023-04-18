7c6eee29711378148a2281019bdee53d84867213
Christian Leyva <chrisley304@hotmail.com>
2023-04-18 16:17:01 -0600

    Final Final changes

--- App/Games/TasksBreak/TasksBreakFinish.swift
+++ App/Games/TasksBreak/TasksBreakFinish.swift
@@ -10,15 +10,22 @@ class TasksBreakFinishScene: SKScene{
         var label = SKLabelNode(fontNamed: "Helvetica")
         label.text = "You hitted all your homeworks!"
         label.fontColor = .green
-        label.fontSize = 30
+        label.fontSize = 20
         label.position = CGPoint(x: size.width/2, y: size.height/2)
         addChild(label)
         
         label = SKLabelNode(fontNamed: "Helvetica")
         label.text = "Keep working hard 💪🏼"
         label.fontColor = .green
-        label.fontSize = 30
+        label.fontSize = 20
         label.position = CGPoint(x: size.width/2, y: size.height/2 - 50)
+        addChild(label)
+        
+        label = SKLabelNode(fontNamed: "Helvetica")
+        label.text = "Touch the close button at the top to close the game. ⬆"
+        label.fontColor = .white
+        label.fontSize = 15
+        label.position = CGPoint(x: size.width/2, y: size.height/2 - 100)
         addChild(label)
     }
     
--- App/Views/IntroView.swift
+++ App/Views/IntroView.swift
@@ -12,11 +12,11 @@ struct IntroView: View {
 
     @State private var pageIndex = 0
     private let pages: [IntroPage] = [
-        IntroPage(name: "Welcome to StudyCompa!", description: "The best companion app for students!", imageUrl: "students-illustration", imageAttrib: "Illustration obtained from: IconScout Free Startup Illustration Pack", tag: 0),
-        IntroPage(name: "Organize your homeworks 📚", description: "Add homework assignments to the app with due dates, class, and priority. Press the \"Give me homework\" button when you're feeling overwhelmed and need help deciding what to work on.", imageUrl: "man-planning-illustration", imageAttrib: "Illustration obtained from: IconScout Free Startup Illustration Pack", tag: 1),
-        IntroPage(name: "Win points with your homeworks 💯", description: "You will receive 10 points for each homework, and if you complete a homework before its due date, you will receive an additional 5 points for each day early.", imageUrl: "planning-man-illustration", imageAttrib: "Illustration obtained from: IconScout Free Startup Illustration Pack", tag: 2),
-        IntroPage(name: "Get fun! 👾", description: "With the points that you win in the app, you can unlock diferent games.", imageUrl: "man-rocket-illustration", imageAttrib: "Illustration obtained from: IconScout Free Startup Illustration Pack", tag: 3),
-        IntroPage(name: "Show your points! 🥇", description: "Customize your StudyCompa card by adding your name and photo. Then, share it with your friends!", imageUrl: "woman-goal-illustration", imageAttrib: "Illustration obtained from: IconScout Free Startup Illustration Pack", tag: 4),
+        IntroPage(name: "Welcome to StudyCompa!", description: "The best companion app for students!", imageUrl: "students-illustration", imageAttrib: "Concept of Business employees team working in business environment - work culture Illustration by IconScout Store", tag: 0),
+        IntroPage(name: "Organize your homeworks 📚", description: "Add homework assignments to the app with due dates, class, and priority. Press the \"Give me homework\" button when you're feeling overwhelmed and need help deciding what to work on.", imageUrl: "man-planning-illustration", imageAttrib: "Multi Tasking ceo handling multiple departments with ease Illustration by IconScout Store", tag: 1),
+        IntroPage(name: "Win points with your homeworks 💯", description: "You will receive 10 points for each homework, and if you complete a homework before its due date, you will receive an additional 5 points for each day early.", imageUrl: "planning-man-illustration", imageAttrib: "Business man illustrate Product Planning option Illustration by IconScout Store", tag: 2),
+        IntroPage(name: "Get fun! 👾", description: "With the points that you win in the app, you can unlock diferent games.", imageUrl: "man-rocket-illustration", imageAttrib: "Man thinking to start a startup Illustration by IconScout Store", tag: 3),
+        IntroPage(name: "Show your points! 🥇", description: "Customize your StudyCompa card by adding your name and photo. Then, share it with your friends!", imageUrl: "woman-goal-illustration", imageAttrib: "Business woman achieving the goal successfully Illustration by IconScout Store", tag: 4),
     ]
     private let dotAppearance = UIPageControl.appearance()
 
